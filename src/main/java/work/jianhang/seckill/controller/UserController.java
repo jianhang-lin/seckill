07af8acc9cadaf5386821f0fad0f84791bf00af9
JianhangLin <linjianhang1010@gmail.com>
2019-06-20 11:33:23 +0800

    实现获取otp逻辑

--- src/main/java/work/jianhang/seckill/controller/UserController.java
+++ src/main/java/work/jianhang/seckill/controller/UserController.java
@@ -1,5 +1,6 @@
 package work.jianhang.seckill.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,19 @@ import work.jianhang.seckill.response.CommonReturnType;
 import work.jianhang.seckill.service.UserService;
 import work.jianhang.seckill.service.model.UserModel;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @GetMapping("/get")
     public CommonReturnType getUser(@RequestParam(name = "id") Integer id) throws BusinessException {
@@ -41,6 +49,23 @@ public class UserController extends BaseController {
         UserVO userVO = new UserVO();
         BeanUtils.copyProperties(userModel, userVO);
         return userVO;
+    }
+
+    // 用户获取otp短信接口
+    @RequestMapping("/getOtp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) {
+        // 需要按照一定的规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+        // 将otp验证码同对应用户的手机号关联,使用httpServletRequest的方式绑定他的手机号与otpCode
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+        // 将otp验证码通过短信通道发送给用户
+        log.info("telphone = {}, otpCode = {}", telphone, otpCode);
+
+        return CommonReturnType.create(null);
     }
 
 }
